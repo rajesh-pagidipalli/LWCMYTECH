cbb4d3f366495e0f60fdb3d7ce4a3ec17655a636
rajesh-pagidipalli <rajeshpagidipalli33@gmail.com>
2025-07-15 16:22:22 +0530

    recaptcha Validations

--- script1.js
+++ script1.js
@@ -1,10 +1,16 @@
-function beforesubmit() {
+let captchachecked = false;
+function beforesubmit(event) {
+    if (captchachecked) {
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value", inputdate.value); // string to date conversion to do (en_US)
 
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
     outputdate.value = formattedDate;
+    } else {
+        alert("Please check the reCAPTCHA box to submit the lead");
+        event.preventDefault();
+    }
 }
 
 function timestamp() { 
@@ -18,3 +24,7 @@ function timestamp() {
     } 
 } 
 setInterval(timestamp, 500);
+
+function captchasuccess() {
+    captchachecked = true;
+}
